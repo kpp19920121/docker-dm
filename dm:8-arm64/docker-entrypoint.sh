376804491ab07a90c906a4327c689217720f86fc
chaz <qinchaozeng@wondersgroup.com>
2022-09-07 16:58:51 +0800

    fix:修正arm64配置容器启动失败的问题 style:格式化xml配置，并修改部分参数

--- dm:8-arm64/docker-entrypoint.sh
+++ dm:8-arm64/docker-entrypoint.sh
@@ -26,7 +26,7 @@ if [ ! -f "${DM_PATH}/bin/DmAPService}" ];then
   ${DM_PATH}/script/root/dm_service_installer.sh -s "${DM_PATH}/bin/DmAPService"
 fi
 if [ ! -f "${DM_PATH}/bin/DmServiceDMSERVER" ];then
-  ${DM_PATH}/script/root/dm_service_installer.sh -t dmserver -p "DMSERVER" -i ${DM_DATA_DIR}/dm.ini
+  ${DM_PATH}/script/root/dm_service_installer.sh -t dmserver -p "DMSERVER" -dm_ini ${DM_DATA_DIR}/dm.ini
 fi
 gosu dmdba ${DM_PATH}/bin/DmAPService start
 gosu dmdba ${DM_PATH}/bin/DmServiceDMSERVER start
